2273e4c27cb002ed5f813fe7325f13b2967ddde3
COURAGE0316 <15037511260@163.com>
2021-09-10 16:29:34 +0800

    创建users子应用

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def log(request):
+    import logging
+
+    # 创建日志记录器
+    logger = logging.getLogger('django')
+    # 输出日志
+    logger.debug('测试logging模块debug')
+    logger.info('测试logging模块info')
+    logger.error('测试logging模块error')
+    return HttpResponse("ok")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('log/', log),
 ]
